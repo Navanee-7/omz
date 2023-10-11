2da11b088099dc9977bab133b2866b6f9158182f
Navanee-7 <nkhellboy7007@gmail.com>
2023-10-11 11:36:39 +0530

    Populated successfully

--- mockup_data/db_populate.py
+++ mockup_data/db_populate.py
@@ -6,7 +6,7 @@ from datetime import datetime
 def post_accesspoints():
     
     #Pre-processing
-    #preprocess_ap()
+    preprocess_ap()
     
     accesspoints_file_path = "csv_files/AccessPoints.csv"
     ap_data = pd.read_csv(accesspoints_file_path)
@@ -25,19 +25,12 @@ def post_accesspoints():
                 print("Failed to populate AccessPoints")
                 print('Response:', response.text)
             ap_json_data.append(row)
-    print(ap_json_data)
-    
-    #POSTing the AccessPoints to the API
-    # for item in ap_json_data:
-    #     response = requests.post(ap_url, json=item)
-    #     if response.status_code == 200:
-    #         print("AccessPoints successfully populated")
-    #         print('Response:', response.json())
-    #     else: 
-    #         print("Failed to populate AccessPoints")
-    #         print('Response:', response.text)
-    
+
 def post_sites():
+    
+    #Pre-processing
+    preprocess_sites()
+    
     sites_file_path = "csv_files/Sites.csv"
     sites_data = pd.read_csv(sites_file_path)
     sites_url = "http://127.0.0.1:8000/api/v1/sites/"
@@ -49,35 +42,29 @@ def post_sites():
         for row in sites_reader:
             
             response = requests.post(sites_url, json=row)
-            if response.status_code == 200:
+            if response.status_code == 201:
                 print("Sites successfully populated")
                 print('Response:', response.json())
             else: 
                 print("Failed to populate Sites")
                 print('Response:', response.text)
             sites_json_data.append(row)
-    #print(sites_json_data[0])
-          
-    #POSTing the Sites to the API
-    # for item in sites_json_data:
-    #     response = requests.post(sites_url, json=item)
-    #     if response.status_code == 200:
-    #         print("Sites successfully populated")
-    #         print('Response:', response.json())
-    #     else: 
-    #         print("Failed to populate Sites")
-    #         print('Response:', response.text)
 
 def preprocess_ap():
     ap_data = pd.read_csv("csv_files/AccessPoints.csv")
-    ap_data['Activated'] = ap_data['Activated'].str[:10]
-    ap_data['Activated'] = pd.to_datetime(ap_data['Activated'],errors="coerce")
+    ap_data['Activated'] = ap_data["Activated"].fillna("2099-01-01")
+    ap_data['Remarks'] = ap_data['Remarks'].fillna("Remarks not available")
     ap_data.to_csv("csv_files/AccessPoints.csv", index=False)
+
+def preprocess_sites():
+    sites_data = pd.read_csv("csv_files/Sites.csv")
+    sites_data['Name'] = sites_data['Name'].fillna("Name of location not available")
+    sites_data['Name'] = sites_data['Name'].replace("0","Name of location not available")
+    sites_data.to_csv("csv_files/Sites.csv", index=False)   
     
 def populate_db():
-    preprocess_ap()
     post_accesspoints()
-    # post_sites()
+    post_sites()
     
 #Main
 def main():
